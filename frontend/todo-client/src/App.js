93fa743b305a998dfb4ae68dc7a20b64848be12f
holyoke <jasonebueng@gmail.com>
2019-07-13 11:00:20 -0700

    introduces frontend todolist component

--- frontend/todo-client/src/App.js
+++ frontend/todo-client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import TodoIndex from './components/TodoList'
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos().then ( (resp)=>{
+      debugger
+    })
+  }, []);
+
   return (
-    <div className="App">
-      Hello mofo
-    </div>
+    <TodoIndex />
   );
 }
 
+export const fetchTodos = () => {
+  const apiUrl = `http://localhost:6969/api/todos`;
+
+  return fetch(apiUrl)
+      .then( (response) => response.json());
+};
+
 export default App;
+
